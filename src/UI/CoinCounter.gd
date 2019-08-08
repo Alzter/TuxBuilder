39a520ef9c23d12871d560b144c815f000a1843d
Alzter <alexsmudgy20@gmail.com>
2019-08-08 23:28:50 +1000

    Add files via upload

--- src/UI/CoinCounter.gd
+++ src/UI/CoinCounter.gd
@@ -1,9 +1,14 @@
 extends Node
 
 var coins = 0
+var coinsmooth = 0
+
+const SMOOTH_FACTOR = 5
 
 func _ready():
 	$CoinCounter/CoinCount.text = "0"
 
-func _update_coin_count():
+func _physics_process(delta):
+	$CoinCounter/CoinCount.rect_size.x = 0
+	$CoinCounter/CoinCount.rect_position.x = -34 - ($CoinCounter/CoinCount.rect_size.x * 0.5)
 	$CoinCounter/CoinCount.text = str(coins)
