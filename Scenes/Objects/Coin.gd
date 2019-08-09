5e8b16b356a8a0bb21107ca41cfedf2e60c316a8
Alzter <alexsmudgy20@gmail.com>
2019-08-09 23:16:02 +1000

    Fixed snowball behaviour

---
+++ Scenes/Objects/Coin.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+var touched = 0
+
+func _on_Coin_body_entered(body):
+	if body.is_in_group("player") and touched == 0:
+		$AnimationPlayer.play("Pickup")
+		var counter = get_tree().get_nodes_in_group("CoinCounter")[0]
+		counter.coins = counter.coins + 1
+		touched = 1
