923fa58be01d099fe1b7e00b4b1c036563dcfb1e
Alzter <alexsmudgy20@gmail.com>
2019-08-14 13:05:16 +1000

    update

--- src/Scenes/Master/Gameplay.gd
+++ src/Scenes/Master/Gameplay.gd
@@ -7,6 +7,20 @@ func _ready():
 	load_level("TEST")
 	load_player()
 	
+func _process(delta):
+	if Input.is_action_just_pressed("click_right"):
+		if editmode == false:
+			editmode = true
+			clear_ui()
+			clear_player()
+			clear_level()
+			load_level(current_level)
+			load_player()
+			load_editor()
+		else:
+			editmode = false
+			clear_editor()
+			load_ui()
 
 func load_level(level):
 	current_level = str(level)
@@ -17,6 +31,29 @@ func load_level(level):
 
 func clear_level():
 	var scene = get_node("Level")
+	remove_child(scene)
+	scene.call_deferred("free")
+
+func load_editor():
+	var scene = load("res://Scenes/Master/LevelEditor.tscn")
+	var scene_instance = scene.instance()
+	scene_instance.set_name("Editor")
+	add_child(scene_instance)
+
+func clear_editor():
+	var scene = get_node("Editor")
+	remove_child(scene)
+
+func load_ui():
+	var scene = load("res://UI/LevelUI.tscn")
+	var scene_instance = scene.instance()
+	scene_instance.set_name("LevelUI")
+	add_child(scene_instance)
+
+func clear_ui():
+	var scene = get_node("LevelUI")
+	for i in get_children():
+		i.queue_free()
 	remove_child(scene)
 	scene.call_deferred("free")
 
@@ -32,29 +69,3 @@ func clear_player():
     i.queue_free()
 	remove_child(scene)
 	scene.call_deferred("free")
-
-func load_ui():
-	var scene = load("res://UI/LevelUI.tscn")
-	var scene_instance = scene.instance()
-	scene_instance.set_name("LevelUI")
-	add_child(scene_instance)
-
-func clear_ui():
-	var scene = get_node("LevelUI")
-	for i in get_children():
-    i.queue_free()
-	remove_child(scene)
-	scene.call_deferred("free")
-
-func _process(delta):
-	if Input.is_action_just_pressed("click_right"):
-		if editmode == false:
-			editmode = true
-			clear_ui()
-			clear_player()
-			clear_level()
-			load_level(current_level)
-			load_player()
-		else:
-			editmode = false
-			load_ui()
