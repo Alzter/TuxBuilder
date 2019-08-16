ccb63c5629c1a7e7b80a15e18f0973eb71f46f43
Alzter <alexsmudgy20@gmail.com>
2019-08-16 20:56:42 +1000

    Level editor tile hover
    A transparent version of the tile you have selected will appear where your cursor is at

--- src/Scenes/Master/LevelEditor.gd
+++ src/Scenes/Master/LevelEditor.gd
@@ -9,6 +9,7 @@ var old_tile_selected = Vector2(0,0)
 var sidebar_offset = 0
 var bottombar_offset = 0
 var swipe_speed = 0
+var mouse_down = false
 
 func _ready():
 	$Grid.visible = false
@@ -62,14 +63,15 @@ func _process(delta):
 	
 	tile_selected = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).world_to_map(get_global_mouse_position())
 	
-	$SelectedTile.position = tile_selected * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size
-	
+	update_selected_tile()
 	
 	if Input.is_action_pressed("click_left"):
 		if get_viewport().get_mouse_position().x < get_viewport().size.x - 128 and get_viewport().get_mouse_position().y < get_viewport().size.y - 64:
-			if tile_selected != old_tile_selected:
+			if tile_selected != old_tile_selected or mouse_down == false:
 				get_tree().current_scene.get_node(str("Level/", tilemap_selected)).set_cellv(tile_selected, tile_type)
 				get_tree().current_scene.get_node(str("Level/", tilemap_selected)).update_bitmask_area(tile_selected)
+		mouse_down = true
+	else: mouse_down = false
 	
 	old_tile_selected = tile_selected
 	
@@ -91,3 +93,5 @@ func update_selected_tile():
 	var selected_texture = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().tile_get_texture(0)
 	$SelectedTile.texture = (selected_texture)
 	$SelectedTile.region_rect.position = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().autotile_get_icon_coordinate(tile_type) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size
+	$SelectedTile.position.x = (tile_selected.x + 0.5) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size.x
+	$SelectedTile.position.y = (tile_selected.y + 0.5) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size.y
