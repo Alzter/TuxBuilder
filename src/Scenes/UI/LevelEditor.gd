fd7d6b0db632a353ce57217a8d93317c75f71709
skyace65 <trekie96@hotmail.com>
2019-08-18 17:30:16 -0400

    Organize level editor scene and code

--- src/Scenes/UI/LevelEditor.gd
+++ src/Scenes/UI/LevelEditor.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 const CAMERA_MOVE_SPEED = 32
-var category_selected = "Tiles"
 var tilemap_selected = "TileMap"
 var tile_type = 0
 var tile_selected = Vector2(0,0)
@@ -23,11 +22,6 @@ func _process(delta):
 	$Grid.rect_position = Vector2(get_tree().current_scene.get_node("Camera2D").position.x - (get_viewport().size.x / 2), get_tree().current_scene.get_node("Camera2D").position.y - (get_viewport().size.y / 2))
 	$Grid.rect_position = Vector2(floor($Grid.rect_position.x / 32) * 32, floor($Grid.rect_position.y / 32) * 32)
 	$Grid.visible = true
-	$UI/SideBar/SideBar.margin_bottom = get_viewport().size.y
-	$UI/BottomBar/BottomBar.margin_right = get_viewport().size.x - 128 + sidebar_offset
-	$UI/SideBar.rect_position.x = get_viewport().size.x + sidebar_offset
-	$UI/SideBarOverlay.rect_position.x = get_viewport().size.x + sidebar_offset
-	$UI/BottomBar.rect_position.y = get_viewport().size.y + (bottombar_offset * 0.5)
 	$Grid.self_modulate = Color(1, 1, 1, 1 - (sidebar_offset / 128))
 	
 	if get_tree().current_scene.editmode == false:
@@ -51,21 +45,12 @@ func _process(delta):
 			bottombar_offset *= 0.8
 		$UI.offset = Vector2(0,0)
 	
-	if $UI/TilesButton.pressed == true:
-		category_selected = "Tiles"
 	
-	if $UI/ObjectsButton.pressed == true:
-		category_selected = "Objects"
 	
-	if category_selected == "Tiles":
-		$UI/SideBarOverlay/TilesSelected.visible = true
-		$UI/SideBarOverlay/ObjectsSelected.visible = false
-	else:
-		$UI/SideBarOverlay/TilesSelected.visible = false
-		$UI/SideBarOverlay/ObjectsSelected.visible = true
+
+func _input(event):
 	
 	tile_selected = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).world_to_map(get_global_mouse_position())
-	
 	update_selected_tile()
 	
 	if Input.is_action_pressed("click_left"):
@@ -87,10 +72,8 @@ func _process(delta):
 	if Input.is_action_pressed("ui_left"):
 		get_tree().current_scene.get_node("Camera2D").position.x -= CAMERA_MOVE_SPEED
 		
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_right"):
 		get_tree().current_scene.get_node("Camera2D").position.x += CAMERA_MOVE_SPEED
-		
-
 
 func update_selected_tile():
 	$SelectedTile.visible = false
@@ -101,3 +84,9 @@ func update_selected_tile():
 		$SelectedTile.region_rect.position = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().autotile_get_icon_coordinate(tile_type) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size
 		$SelectedTile.position.x = (tile_selected.x + 0.5) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size.x
 		$SelectedTile.position.y = (tile_selected.y + 0.5) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size.y
+
+func _on_TilesButton_pressed():
+	pass
+
+func _on_ObjectsButton_pressed():
+	pass
