335a633f242a6df085f08aac6f47d941e2e89a65
Alzter <alexsmudgy20@gmail.com>
2019-08-19 19:30:51 +1000

    Eraser tool
    Now you can erase stuff (finally) in the level editor

--- src/Scenes/UI/LevelEditor.gd
+++ src/Scenes/UI/LevelEditor.gd
@@ -49,33 +49,38 @@ func _process(delta):
 		
 	if Input.is_action_pressed("ui_right"):
 		get_tree().current_scene.get_node("Camera2D").position.x += CAMERA_MOVE_SPEED
-
-func _input(event):
-	if get_tree().current_scene.editmode == false:
-		return
 	
+	# Placing tiles
 	tile_selected = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).world_to_map(get_global_mouse_position())
 	update_selected_tile()
-	
 	if Input.is_action_pressed("click_left"):
 		if get_viewport().get_mouse_position().x < get_viewport().size.x - 128 and get_viewport().get_mouse_position().y < get_viewport().size.y - 64:
 			if tile_selected != old_tile_selected or mouse_down == false:
-				get_tree().current_scene.get_node(str("Level/", tilemap_selected)).set_cellv(tile_selected, tile_type)
+				if $UI/SideBar/VBoxContainer/HBoxContainer/EraserButton.pressed == true:
+					get_tree().current_scene.get_node(str("Level/", tilemap_selected)).set_cellv(tile_selected, -1)
+				else: get_tree().current_scene.get_node(str("Level/", tilemap_selected)).set_cellv(tile_selected, tile_type)
 				get_tree().current_scene.get_node(str("Level/", tilemap_selected)).update_bitmask_area(tile_selected)
 		mouse_down = true
 	else: mouse_down = false
-	
 	old_tile_selected = tile_selected
 
 func update_selected_tile():
-	$SelectedTile.visible = false
+	$SelectedTile.visible = true
 	if get_viewport().get_mouse_position().x < get_viewport().size.x - 128 and get_viewport().get_mouse_position().y < get_viewport().size.y - 64:
-		$SelectedTile.visible = true
-		var selected_texture = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().tile_get_texture(0)
-		$SelectedTile.texture = (selected_texture)
-		$SelectedTile.region_rect.position = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().autotile_get_icon_coordinate(tile_type) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size
+		if $UI/SideBar/VBoxContainer/HBoxContainer/EraserButton.pressed == true:
+			$SelectedTile.texture = load("res://Sprites/Editor/EraseSelect.png")
+			$SelectedTile.region_rect = Rect2(0,0,32,32)
+			$SelectedTile.modulate = Color(1,1,1,1)
+			$EraserSprite.visible = true
+		else:
+			var selected_texture = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().tile_get_texture(0)
+			$SelectedTile.texture = (selected_texture)
+			$SelectedTile.region_rect.position = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().autotile_get_icon_coordinate(tile_type) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size
+			$SelectedTile.modulate = Color(1,1,1,0.25)
+			$EraserSprite.visible = false
 		$SelectedTile.position.x = (tile_selected.x + 0.5) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size.x
 		$SelectedTile.position.y = (tile_selected.y + 0.5) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size.y
+		$EraserSprite.position = $SelectedTile.position
 
 # Buttons
 func _on_TilesButton_pressed():
