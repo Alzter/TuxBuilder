f147ef40a6607411e5165ae3578c175e273931e9
Alzter <alexsmudgy20@gmail.com>
2019-08-29 16:57:54 +1000

    Wip rectangle select

--- src/Scenes/UI/LevelEditor.gd
+++ src/Scenes/UI/LevelEditor.gd
@@ -10,6 +10,7 @@ var object_category = "BadGuys"
 var object_type = "Snowball"
 var mouse_down = false
 var anim_in = false
+var rect_start_pos = Vector2() # Where you started clicking for the rectangle select
 
 var layer_types = [] # All layer types (Gets from layer folder)
 
@@ -68,6 +69,7 @@ func _process(_delta):
 	# Placing tiles / objects
 	tile_selected = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).world_to_map(get_global_mouse_position())
 	update_selected_tile()
+	
 	if Input.is_action_pressed("click_left"):
 		if get_viewport().get_mouse_position().x < get_viewport().size.x - 128 and get_viewport().get_mouse_position().y < get_viewport().size.y - 64 and (tile_selected != old_tile_selected or mouse_down == false) and $UI/AddLayer.visible == false:
 			
@@ -107,11 +109,29 @@ func update_selected_tile():
 		$SelectedTile.visible = false
 		return
 	
-	$SelectedTile.position.x = (tile_selected.x + 0.5) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size.x
-	$SelectedTile.position.y = (tile_selected.y + 0.5) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size.y
+	var cellsize = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size
+	
+	$SelectedTile.position.x = (tile_selected.x + 0.5) * cellsize.x
+	$SelectedTile.position.y = (tile_selected.y + 0.5) * cellsize.y
 	$SelectedTile.offset = Vector2(0,0)
 	
-	if $UI/SideBar/VBoxContainer/HBoxContainer/EraserButton.pressed == true:
+	# Rectangle selection
+	$SelectedArea.visible == false
+	if $UI/SideBar/VBoxContainer/HBoxContainer/SelectButton.pressed == true and Input.is_action_pressed("click_left"):
+		# Start rectangle selection
+		if Input.is_action_just_pressed("click_left"):
+			rect_start_pos = tile_selected
+		
+		$SelectedArea.rect_position.x = (rect_start_pos.x) * cellsize.x
+		$SelectedArea.rect_position.y = (rect_start_pos.y) * cellsize.y
+		$SelectedArea.rect_scale.x = (-1 * ($SelectedArea.rect_position.x - ($SelectedTile.position.x))) / 32
+		$SelectedArea.rect_scale.y = (-1 * ($SelectedArea.rect_position.y - ($SelectedTile.position.y))) / 32
+		$SelectedArea.visible == true
+		$EraserSprite.visible = false
+		$SelectedTile.visible = false
+	
+	elif $UI/SideBar/VBoxContainer/HBoxContainer/EraserButton.pressed == true:
+		$SelectedArea.color = Color(1,0,0,0.5)
 		$EraserSprite.visible = true
 		$SelectedTile.visible = true
 		$SelectedTile.texture = load("res://Sprites/Editor/EraseSelect.png")
@@ -120,6 +140,7 @@ func update_selected_tile():
 		$EraserSprite.position = $SelectedTile.position
 	
 	else:
+		$SelectedArea.color = Color(0,1,0,0.5)
 		$EraserSprite.visible = false
 		$SelectedTile.visible = true
 		$SelectedTile.modulate = Color(1,1,1,0.25)
@@ -127,7 +148,7 @@ func update_selected_tile():
 		if category_selected == "Tiles":
 			var selected_texture = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().tile_get_texture(tile_type)
 			$SelectedTile.texture = (selected_texture)
-			$SelectedTile.region_rect.position = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().autotile_get_icon_coordinate(tile_type) * get_tree().current_scene.get_node(str("Level/", tilemap_selected)).cell_size
+			$SelectedTile.region_rect.position = get_tree().current_scene.get_node(str("Level/", tilemap_selected)).get_tileset().autotile_get_icon_coordinate(tile_type) * cellsize
 			$SelectedTile.region_enabled = true
 
 		else:
