98b69d2f615499a9e968e45ad1e38f78c94b90f4
Alzter <alexsmudgy20@gmail.com>
2019-08-30 23:30:53 +1000

    spawnpoint update

--- src/Scenes/Objects/Misc/SpawnPoint.gd
+++ src/Scenes/Objects/Misc/SpawnPoint.gd
@@ -1,9 +1,8 @@
 extends Position2D
 
-func _ready():
-	set_name("SpawnPoint")
-	if get_name() != ("SpawnPoint"):
-		queue_free()
-
 func _process(_delta):
 	visible = get_tree().current_scene.editmode
+	set_name("SpawnPoint")
+	if get_name() != ("SpawnPoint"):
+		get_tree().current_scene.get_node("Level/SpawnPoint").queue_free()
+		set_name("SpawnPoint")
