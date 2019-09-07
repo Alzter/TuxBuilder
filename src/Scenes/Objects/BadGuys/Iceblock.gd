0ca66399f41817550a6cc30b6dc921f59ab6a69a
Tobias Markus <tobbi.bugs@googlemail.com>
2019-09-07 12:56:03 +0200

    Refactor a little bit

--- src/Scenes/Objects/BadGuys/Iceblock.gd
+++ src/Scenes/Objects/BadGuys/Iceblock.gd
@@ -1,52 +1,27 @@
-extends KinematicBody2D
+extends "BadGuy.gd"
 
-const FLOOR = Vector2(0, -1)
-
-var velocity = Vector2(0,0)
-var startpos = Vector2(0,0)
-var state = "active"
-var direction = 1
-var rotate = 0
 var invincible_time = 0
 
 const INVINCIBLE_TIME = 10
 const KICK_SPEED = 500
 
-func _ready():
-	startpos = position
-	direction = $Control/AnimatedSprite.scale.x
+func on_ready():
 	collision_mask = 2
-
-func disable():
-	remove_from_group("badguys")
-	$CollisionShape2D.call_deferred("set_disabled", true)
-	$Head/CollisionShape2D.call_deferred("set_disabled", true)
-	$Area2D/CollisionShape2D.call_deferred("set_disabled", true)
+	
+func _squish(delta):
+	velocity.y += 20
+	velocity = move_and_slide(velocity, FLOOR)
 
 # Physics
-func _physics_process(delta):
+func on_physics_process(delta):
+
+	if invincible_time > 0: 
+		invincible_time -= 1
 	
-	if invincible_time > 0: invincible_time -= 1
-	
-	if get_tree().current_scene.editmode == true:
-		return
-	
-	
-	if state != "kicked": collision_layer = 2
-	else: collision_layer = 8
-	
-	# Movement
-	if state == "active":
-		velocity.x = -100 * $Control/AnimatedSprite.scale.x
-		velocity.y += 20
-		velocity = move_and_slide(velocity, FLOOR)
-		if is_on_wall():
-			$Control/AnimatedSprite.scale.x *= -1
-			velocity.x *= -1
-	
-	if state == "squished":
-		velocity.y += 20
-		velocity = move_and_slide(velocity, FLOOR)
+	if state != "kicked": 
+		collision_layer = 2
+	else: 
+		collision_layer = 8
 	
 	if state == "kicked":
 		velocity.x = KICK_SPEED * -$Control/AnimatedSprite.scale.x
@@ -57,29 +32,11 @@ func _physics_process(delta):
 			$Control/AnimatedSprite.scale.x *= -1
 			velocity.x *= -1
 			$SFX/Bump.play()
-	
-	# Kill states
-	if state == "kill":
-		position += velocity * delta
-		velocity.y += 20
-		$Control/AnimatedSprite.rotation_degrees += rotate
 
 # Custom fireball death animation (optional)
-func fireball_kill():
-	disable()
-	state = ""
+func on_fireball_kill():
 	$SFX/Melt.play()
 	$AnimationPlayer.play("explode")
-	
-# If hit by bullet or invincible player
-func kill():
-	disable()
-	$AnimationPlayer.stop()
-	state = "kill"
-	if velocity.x == 0: velocity.x = 1
-	velocity = Vector2(300 * (velocity.x / abs(velocity.x)), -350)
-	rotate = 30 * (velocity.x / abs(velocity.x))
-	$SFX/Fall.play()
 
 # If squished
 func _on_Head_area_entered(area):
@@ -153,13 +110,6 @@ func throw():
 	state = "kicked"
 	$Control/AnimatedSprite.play("squished")
 	$SFX/Kick.play()
-
-# Die when knocked off stage
-func _on_VisibilityEnabler2D_screen_exited():
-	if state == "kill" or state == "": queue_free()
-
-func appear(dir):
-	$Control/AnimatedSprite.scale.x = -dir
 
 func _on_WakeupTimer_timeout():
 	$WakeupTimer.stop()
