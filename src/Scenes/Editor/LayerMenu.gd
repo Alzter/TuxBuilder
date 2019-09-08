fb8206af8e3dc4499db47c67955995a289ee03da
Alzter <alexsmudgy20@gmail.com>
2019-09-08 13:18:15 +1000

    Scroll and move speed

--- src/Scenes/Editor/LayerMenu.gd
+++ src/Scenes/Editor/LayerMenu.gd
@@ -5,11 +5,15 @@ var layername = ""
 var layername2 = ""
 var z_axis = 0
 var hide = false
+var layer = null # Where to get the layer from
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Stop editor doing stuff
 	get_tree().current_scene.get_node("Editor").stop = true
+	
+	# Get the layer
+	layer = get_tree().current_scene.get_node(str("Level/", layername))
 	
 	# Set name and Z axis
 	$Popup/Panel/VBoxContainer/Name/LineEdit.text = layername
@@ -19,12 +23,19 @@ func _ready():
 	if not (type == "TileMap"):
 		$Popup/Panel/VBoxContainer/Solid/CheckBox.disabled = true
 		$Popup/Panel/VBoxContainer/Solid/CheckBox.pressed = false
-	elif get_tree().current_scene.get_node(str("Level/", layername)).get_collision_layer() != 0:
+	elif layer.get_collision_layer() != 0:
 		$Popup/Panel/VBoxContainer/Solid/CheckBox.pressed = true
 	else: $Popup/Panel/VBoxContainer/Solid/CheckBox.pressed = false
 	
 	# Set tint box
-	$Popup/Panel/VBoxContainer/Tint/ColorPickerButton.color = get_tree().current_scene.get_node(str("Level/", layername)).modulate
+	$Popup/Panel/VBoxContainer/Tint/ColorPickerButton.color = layer.modulate
+	
+	# Set scroll and move speed
+	$Popup/Panel/VBoxContainer/ScrollX/SpinBox.value = layer.scroll_speed.x
+	$Popup/Panel/VBoxContainer/ScrollY/SpinBox.value = layer.scroll_speed.y
+	$Popup/Panel/VBoxContainer/MoveX/SpinBox.value = layer.move_speed.x
+	$Popup/Panel/VBoxContainer/MoveY/SpinBox.value = layer.move_speed.y
+	$Popup/Panel/VBoxContainer/Moving/CheckBox.pressed = layer.moving
 	
 	$Popup.popup()
 
@@ -32,17 +43,18 @@ func _process(_delta):
 	
 	# Change layer name
 	layername2 = $Popup/Panel/VBoxContainer/Name/LineEdit.text
-	if get_tree().current_scene.get_node(str("Level/", layername)).get_name() != layername2:
+	if layer.get_name() != layername2:
 		get_tree().current_scene.get_node(str("Editor/UI/BottomBar/ScrollContainer/HBoxContainer/", layername)).layername = layername2
-		get_tree().current_scene.get_node(str("Level/", layername)).set_name(layername2)
+		layer.set_name(layername2)
 		if get_tree().current_scene.get_node("Editor").layer_selected == layername:
 			get_tree().current_scene.get_node("Editor").layer_selected = layername2
 		layername = layername2
+		layer = get_tree().current_scene.get_node(str("Level/", layername))
 	
 	# Change layer z axis
 	z_axis = $Popup/Panel/VBoxContainer/Zaxis/SpinBox.value
-	if get_tree().current_scene.get_node(str("Level/", layername)).z_index != z_axis:
-		get_tree().current_scene.get_node(str("Level/", layername)).z_index = z_axis
+	if layer.z_index != z_axis:
+		layer.z_index = z_axis
 		get_tree().current_scene.get_node(str("Editor/UI/BottomBar/ScrollContainer/HBoxContainer/", layername)).z_axis = z_axis
 	
 	# Change layer solidity
@@ -50,14 +62,21 @@ func _process(_delta):
 		$Popup/Panel/VBoxContainer/Solid/CheckBox.disabled = true
 		$Popup/Panel/VBoxContainer/Solid/CheckBox.pressed = false
 	elif $Popup/Panel/VBoxContainer/Solid/CheckBox.pressed == true:
-		get_tree().current_scene.get_node(str("Level/", layername)).set_collision_layer(31)
-		get_tree().current_scene.get_node(str("Level/", layername)).set_collision_mask(31)
+		layer.set_collision_layer(31)
+		layer.set_collision_mask(31)
 	else:
-		get_tree().current_scene.get_node(str("Level/", layername)).set_collision_layer(0)
-		get_tree().current_scene.get_node(str("Level/", layername)).set_collision_mask(0)
+		layer.set_collision_layer(0)
+		layer.set_collision_mask(0)
 	
 	# Change layer tint
-	get_tree().current_scene.get_node(str("Level/", layername)).modulate = $Popup/Panel/VBoxContainer/Tint/ColorPickerButton.color
+	layer.modulate = $Popup/Panel/VBoxContainer/Tint/ColorPickerButton.color
+	
+	# Change layer scroll and move speed
+	layer.scroll_speed.x = $Popup/Panel/VBoxContainer/ScrollX/SpinBox.value
+	layer.scroll_speed.y = $Popup/Panel/VBoxContainer/ScrollY/SpinBox.value
+	layer.move_speed.x = $Popup/Panel/VBoxContainer/MoveX/SpinBox.value
+	layer.move_speed.y = $Popup/Panel/VBoxContainer/MoveY/SpinBox.value
+	layer.moving = $Popup/Panel/VBoxContainer/Moving/CheckBox.pressed
 	
 	# Delete if not in edit mode
 	if get_tree().current_scene.editmode == false:
@@ -83,7 +102,7 @@ func _on_DeleteYes_pressed():
 		get_tree().current_scene.get_node("Editor").layer_selected = ""
 		get_tree().current_scene.get_node("Editor").layer_selected_type = ""
 	get_tree().current_scene.get_node(str("Editor/UI/BottomBar/ScrollContainer/HBoxContainer/", layername)).queue_free()
-	get_tree().current_scene.get_node(str("Level/", layername)).queue_free()
+	layer.queue_free()
 	get_tree().current_scene.get_node("Editor").stop = false
 	queue_free()
 
---
+++ src/Scenes/Editor/Layers/Layer.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+export var scroll_speed = Vector2(1,1)
+export var move_speed = Vector2()
+export var moving = false
+var move_pos = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	position = Vector2(0,0)
+	
+	# Scrolling
+	if scroll_speed.x != 1:
+		position.x = get_tree().current_scene.get_node("Camera2D").position.x * (1 - scroll_speed.x)
+	if scroll_speed.y != 1:
+		position.x = get_tree().current_scene.get_node("Camera2D").position.y * (1 - scroll_speed.x)
+	
+	# Moving
+	if moving and get_tree().current_scene.editmode == false:
+		if move_speed.x != 0:
+			move_pos.x += move_speed.x
+		if move_speed.y != 0:
+			move_pos.y += move_speed.y
+	
+	position += move_pos
