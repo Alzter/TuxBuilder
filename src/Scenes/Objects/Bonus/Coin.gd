f05bb92469611c90a108d122db8758ef7319612c
Tobias Markus <tobbi.bugs@googlemail.com>
2019-09-14 19:13:23 +0200

    Make touched a boolean variable

--- src/Scenes/Objects/Bonus/Coin.gd
+++ src/Scenes/Objects/Bonus/Coin.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
-var touched = 0
+var touched = false
 
 func _on_Coin_body_entered(body):
-	if body.is_in_group("player") and touched == 0:
+	if body.is_in_group("player") and not touched:
 		$AnimationPlayer.play("Pickup")
 		var counter = get_tree().get_nodes_in_group("CoinCounter")[0]
 		counter.coins = counter.coins + 1
-		touched = 1
+		touched = true
 
 func appear(dir, hitdown):
 	$AnimationPlayer.play("Pickup")
 	var counter = get_tree().get_nodes_in_group("CoinCounter")[0]
 	counter.coins = counter.coins + 1
-	touched = 1
+	touched = true
