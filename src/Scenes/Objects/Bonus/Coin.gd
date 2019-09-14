b178c77463c1fd585e689d27d1133ea53e336036
Tobias Markus <tobbi.bugs@googlemail.com>
2019-09-14 19:16:41 +0200

    Use += 1 instead of referencing variable more than once Move collect code to its own function

--- src/Scenes/Objects/Bonus/Coin.gd
+++ src/Scenes/Objects/Bonus/Coin.gd
@@ -2,15 +2,15 @@ extends Area2D
 
 var touched = false
 
-func _on_Coin_body_entered(body):
-	if body.is_in_group("player") and not touched:
-		$AnimationPlayer.play("Pickup")
-		var counter = get_tree().get_nodes_in_group("CoinCounter")[0]
-		counter.coins = counter.coins + 1
-		touched = true
-
-func appear(dir, hitdown):
+func collect():
 	$AnimationPlayer.play("Pickup")
 	var counter = get_tree().get_nodes_in_group("CoinCounter")[0]
-	counter.coins = counter.coins + 1
+	counter.coins += 1
 	touched = true
+
+func _on_Coin_body_entered(body):
+	if body.is_in_group("player") and not touched:
+		collect()
+
+func appear(dir, hitdown):
+	collect()
