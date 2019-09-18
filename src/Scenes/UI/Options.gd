cb72d0a94afc00cdcc3bf8ded540a7c2d218223e
Tobias Markus <tobbi.bugs@googlemail.com>
2019-09-19 00:21:16 +0200

    Simplify option toggling

--- src/Scenes/UI/Options.gd
+++ src/Scenes/UI/Options.gd
@@ -1,22 +1,16 @@
 extends Popup
 
 func _ready():
-	if OS.window_fullscreen == true:
+	if OS.window_fullscreen:
 		$VBoxContainer/HBoxContainer/FullscreenCheck.pressed = true
-	if OS.vsync_enabled == true:
+	if OS.vsync_enabled:
 		$VBoxContainer/HBoxContainer2/VSyncCheck.pressed = true
 
 func _on_FullscreenCheck_pressed():
-	if OS.window_fullscreen == true:
-		OS.window_fullscreen = false
-	else:
-		OS.window_fullscreen = true
+	OS.window_fullscreen = !OS.window_fullscreen
 
 func _on_VSyncCheck_pressed():
-	if OS.vsync_enabled == true:
-		OS.vsync_enabled = false
-	else:
-		OS.vsync_enabled = true
+	OS.vsync_enabled = !OS.vsync_enabled
 
 func _on_Back_pressed():
 	get_parent().get_node("Panel").show()
