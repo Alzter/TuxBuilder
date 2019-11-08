0e3e524d6ee4958fe8d3f97db202f692f9133bd0
Alzter <alexsmudgy20@gmail.com>
2019-11-08 22:39:05 +1100

    WIP path navigation

--- src/Scenes/Player/Worldmap.gd
+++ src/Scenes/Player/Worldmap.gd
@@ -3,6 +3,9 @@ extends Node2D
 var dead = false
 var state = "big"
 var moving = false
+var direction = 180
+
+const MOVE_SPEED = 8
 
 func _ready():
 	position = Vector2(0,0)
@@ -14,10 +17,33 @@ func _process(delta):
 	if UIHelpers._get_scene().editmode:
 		return
 	
-	for child in UIHelpers.get_level().get_children():
-		if child.is_in_group("tilemap"):
-			var playerpos = child.world_to_map(UIHelpers.get_player().position)
-			var tile_id = child.get_cellv(playerpos)
-			var tile_name = child.get_tileset().tile_get_name(tile_id)
-			if tile_name == "Pathing":
-				pass
+	# Setting the direction to move
+	if Input.is_action_just_pressed("up"):
+		moving = true
+		direction = 0
+	
+	if Input.is_action_just_pressed("duck"):
+		moving = true
+		direction = 180
+	
+	if Input.is_action_just_pressed("move_left"):
+		moving = true
+		direction = -90
+	
+	if Input.is_action_just_pressed("move_right"):
+		moving = true
+		direction = 90
+	
+	# Moving across the path
+	if moving:
+		for child in UIHelpers.get_level().get_children():
+			if child.is_in_group("tilemap"):
+				var playerpos = child.world_to_map(UIHelpers.get_player().position)
+				var tile_id = child.get_cellv(playerpos)
+				var tile_name = child.get_tileset().tile_get_name(tile_id)
+				
+				if tile_name == "Pathing":
+					var tile_pos = child.get_cell_autotile_coord(playerpos.x, playerpos.y)
+					
+					var up_tiles = []
+					var down_tiles = [Vector2(2,0), Vector2(3,0), Vector2(3,1), Vector2(3,2), Vector2(2,3)]
